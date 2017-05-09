30a69998b817d641fbbf87b08e8cd913bd343674
CampioniMan <campioni.daniel@gmail.com>
2017-05-09 18:48:49 -0300

    Fundo da água e Menu
    Primeira versão do menu, sem nenhum tipo de verificação ou validação. Fundo dá água tem o efeito um pouco alterado.

---
+++ dev-VR/THG_VR/Assets/Scripts/ScriptMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptMenu : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Sair()
+    {
+        // fechar o socket de conexão, se tiver(vai que)
+        // parar o áudio
+
+        // realmente sair
+        Application.Quit();
+    }
+}
