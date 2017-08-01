cc6e5a9cacbb61eeba3f13cebd96b2e9ad0ce3c3
REGIS GABETTA DE SOUZA <regis_gabetta@hotmail.com>
2017-08-01 11:03:28 -0300

    organização do código da´física do personagem e da arma

--- dev-VR/THG_VR/Assets/Scripts/CatchableObject.cs
+++ dev-VR/THG_VR/Assets/Scripts/CatchableObject.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class CatchableObject : MonoBehaviour {
 
-    public bool taComArma;
-    private Vector3 moveDirection = Vector3.zero;
+    private bool taComArma;
+    private Transform balaAtual;
     private Transform[] balas;
     public Transform bala1;
     public Transform bala2;
@@ -16,35 +16,11 @@ public class CatchableObject : MonoBehaviour {
     public Transform bala8;
     public Transform bala9;
     public Transform bala10;
-    private Transform balaGeral;
-    private int[] tempo;
-    private bool caught;
     public Transform target;
-    private object someScript;
-//    Rigidbody bodyBala;
-    
-    
-    // Use this for initialization
-    void Start () {
-        caught = false;
-        tempo = new int[10];
-        balas = new Transform[10];
-        balas[0] = bala1;
-        balas[1] = bala2;
-        balas[2] = bala3;
-        balas[3] = bala4;
-        balas[4] = bala5;
-        balas[5] = bala6;
-        balas[6] = bala7;
-        balas[7] = bala8;
-        balas[8] = bala9;
-        balas[9] = bala10;
-        balaGeral = balas[0];
-        //taComArma = false;
-	}
+
 
     
-    void moverBalas()
+    void moverBalasAtiradas()
     {
         for(int i = 0;i<10;i++)
         {
@@ -57,89 +33,124 @@ public class CatchableObject : MonoBehaviour {
         }
     }
 
-    void prepararBala()
+    void conectarBalasNoVetor()
+    {
+        balas[0] = bala1;
+        balas[1] = bala2;
+        balas[2] = bala3;
+        balas[3] = bala4;
+        balas[4] = bala5;
+        balas[5] = bala6;
+        balas[6] = bala7;
+        balas[7] = bala8;
+        balas[8] = bala9;
+        balas[9] = bala10;
+    }
+
+    void prepararBalaAtual()
     {
         for (int i = 0; i < 10; i++)
         {
             if (balas[i].transform.position == new Vector3(50, 160, 90))
             {
-                balaGeral = balas[i];
+                balaAtual = balas[i];
             }
         }
+
+        float val1 = GameObject.FindGameObjectWithTag("VrMain").transform.rotation.eulerAngles.y;
+        float val2 = GameObject.FindGameObjectWithTag("HeadTag").transform.rotation.eulerAngles.y - val1;
+        float val3 = val2 + val1;
+
+        float valX = GameObject.FindGameObjectWithTag("HeadTag").transform.rotation.eulerAngles.x;
+
+        balaAtual.transform.eulerAngles = new Vector3(valX, val3, balaAtual.transform.rotation.z);
+    }
+
+    bool apertouBotaoDeTiro()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    bool podePegarArma()
+    {
+        return Physics.Raycast(target.transform.position, GameObject.FindGameObjectWithTag("MainCamera").transform.forward, 1);
+    }
+
+    bool apertouTab()
+    {
+        return Input.GetKeyDown(KeyCode.Tab);
     }
     void atirar()
     {
-        balaGeral.GetComponent<Rigidbody>().isKinematic = true;
-        balaGeral.GetComponent<Rigidbody>().useGravity = false;
-
-        balaGeral.transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
-        balaGeral.transform.localPosition = new Vector3(1.26f, -1.1f, 1.2f);   
+        balaAtual.GetComponent<Rigidbody>().isKinematic = true;
+        balaAtual.GetComponent<Rigidbody>().useGravity = false;
+        balaAtual.transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        balaAtual.transform.localPosition = new Vector3(1.26f, -1.1f, 1.2f);   
     }
 
-    // Update is called once per frame
-
-    void Update()
+    void pegarArma()
     {
-        moverBalas();
-            
-        if (taComArma)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                float val1 = GameObject.FindGameObjectWithTag("VrMain").transform.rotation.eulerAngles.y;
-                float val2 = GameObject.FindGameObjectWithTag("HeadTag").transform.rotation.eulerAngles.y - val1;
-                float val3 = val2 + val1;
-                //Debug.Log("Val 1: " + val1);
-                //Debug.Log("Val 2: " + val2);
-                //Debug.Log("Val 3: " + val3);
-                float valX = GameObject.FindGameObjectWithTag("HeadTag").transform.rotation.eulerAngles.x;
-                prepararBala();
-                balaGeral.transform.eulerAngles = new Vector3(valX,val3,balaGeral.transform.rotation.z);
+        this.GetComponent<Rigidbody>().isKinematic = true;
+        this.GetComponent<Rigidbody>().useGravity = false;
 
-                
-                //float VarX = GameObject.FindGameObjectWithTag("HeadTag").transform.rotation.eulerAngles.x;
-                //Debug.Log("X : " + VarX);
-                //balaGeral.transform.rotation = Quaternion.AngleAxis(VarX, Vector3.right);
-                atirar();
-               
-            }
+        this.transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        this.transform.localPosition = new Vector3(1.26f, -1.1f, 1.2f);
+        taComArma = true;
+    }
 
-            
-        }
+    void soltarArma()
+    {
+        this.transform.parent = GameObject.Find("VRMain").transform;
+        this.transform.parent = null;
+        this.GetComponent<Rigidbody>().isKinematic = false;
+        this.GetComponent<Rigidbody>().useGravity = true;
+        taComArma = false;
+    }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(target.transform.position, GameObject.FindGameObjectWithTag("MainCamera").transform.forward, 1))
-            {
-                this.GetComponent<Rigidbody>().isKinematic = true;
-                this.GetComponent<Rigidbody>().useGravity = false;
-
-                this.transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
-                this.transform.localPosition = new Vector3(1.26f, -1.1f, 1.2f);
-                taComArma = true;     
-            }
-
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            this.transform.parent = GameObject.Find("VRMain").transform;
-            this.transform.parent = null;
-            this.GetComponent<Rigidbody>().isKinematic = false;
-            this.GetComponent<Rigidbody>().useGravity = true;
-            taComArma = false;
-        }
-
+    void aumentarGravidade()
+    {
         CharacterController controller = GetComponent<CharacterController>();
 
 
         Rigidbody body;
         body = gameObject.GetComponent<Rigidbody>();
 
-        if(!controller.isGrounded)
-            body.AddForce(new Vector3(0,-100f, 0));
+        if (!controller.isGrounded)
+            body.AddForce(new Vector3(0, -100f, 0));
+    }
 
+    void Start()
+    {
+        taComArma = false;
+        balas = new Transform[10];
+        conectarBalasNoVetor();
+        balaAtual = balas[0];
+    }
+
+    void Update()
+    {
+        moverBalasAtiradas();
+                    
+        if (taComArma && apertouBotaoDeTiro())
+        {
+                prepararBalaAtual();
+                atirar();            
+        }
+        else
+        if (apertouBotaoDeTiro())
+        {
+            if (podePegarArma())
+            {
+                pegarArma();                
+            }
+        }
+
+        if (apertouTab() && taComArma)
+        {
+            soltarArma();
+        }
+
+        aumentarGravidade();
     }
 
     
--- dev-VR/THG_VR/Assets/Scripts/Controller.cs
+++ dev-VR/THG_VR/Assets/Scripts/Controller.cs
@@ -5,74 +5,89 @@ using System.Collections;
 public class controller : MonoBehaviour
 {
     public float speed = 3.0F;
+    private const float MAX_SPEED = 15.0F;
+    private const float MIN_SPEED = 3.0F;
     public float rotateSpeed = 3.0F;
     private Vector3 moveDirection = Vector3.zero;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
-//    private bool rotacionando = false;
+    public const float JUMP_SPEED = 15.0F;
+    public const float GRAVITY= 20.0F;
     private Vector3 spawnPoint;
 
-    void Start()
+    bool apertouBotaoCorrer()
     {
-        spawnPoint = transform.position;
+        return Input.GetKeyDown(KeyCode.LeftShift);
     }
-    void Update()
+    
+    bool soltouBotaoCorrer()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            speed = 15.0F;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            speed = 3.0F;
-        }
-        //if(Input.GetKey(KeyCode.LeftAlt))
-        //{
-        //    rotacionando = true;
-        //}
-        //if (Input.GetKeyUp(KeyCode.LeftAlt))
-        //{
-        //    rotacionando = false;
-        //}
+        return Input.GetKeyUp(KeyCode.LeftShift);
+    }
 
-        //if(rotacionando)
-        //{
-        //    float y = this.transform.localPosition.y + transform.Find("Head").localPosition.y;
-        //    this.transform.localPosition = new Vector3(this.transform.localPosition.x,y, this.transform.localPosition.z);
-        //}
+    bool podePular()
+    {
+        return Physics.Raycast(transform.position, transform.up, -1);
+    }
 
+    void pular()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveDirection.y = JUMP_SPEED;
+        }
+    }
+
+    void adicionarGravidade()
+    {
+        moveDirection.y -= GRAVITY * Time.deltaTime;
+    }
+
+    void moverPersonagem()
+    {
         CharacterController controller = GetComponent<CharacterController>();
-
-        if (Physics.Raycast(transform.position, transform.up,-1))
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
-
         controller.Move(moveDirection * Time.deltaTime);
 
         transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
         controller.SimpleMove(Camera.main.transform.forward * curSpeed);
+    }
 
-        if (transform.position.y < -10f)
+    void respawn()
+    {
+        transform.position = new Vector3(spawnPoint.x + 50, spawnPoint.y, spawnPoint.z + 50);
+    }
+
+
+    void Start()
+    {
+        spawnPoint = transform.position;
+    }
+
+    void Update()
+    {
+        if (apertouBotaoCorrer())
         {
-            transform.position = new Vector3(spawnPoint.x + 50,spawnPoint.y,spawnPoint.z+50);
+            speed = MAX_SPEED;
         }
-    }
+        else
+        if (soltouBotaoCorrer())
+        {
+            speed = MIN_SPEED;
+        }
 
-    public Vector3 getCameraForward()
-    {
-        return Camera.main.transform.forward;
-    }
+        if (podePular())
+        {
+            pular();
+        }
 
-    public Vector3 getTransformPosition()
-    {
-        return Camera.main.transform.position;
+        adicionarGravidade();
+
+        moverPersonagem();    
+
+        if (transform.position.y < -50f)
+        {
+            respawn();
+        }
     }
 
 }
