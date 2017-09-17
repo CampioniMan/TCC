a3e7f1eceacc61248835551587f7a9514cd3de2d
CampioniMan <campioni.daniel@gmail.com>
2017-09-17 02:17:50 -0300

    AI dos inimigos
    Agora há 3 arquivos que fazem a busca do caminho mais perto e executam ela (sando A*) basenado-se no NavMesh do terreno/coisa que serve de chão.

---
+++ dev-VR/THG_VR/Assets/Editor/AIWaypointNetwork.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum PathDisplayMode { None, Connections, Paths }
+public class AIWaypointNetwork : MonoBehaviour 
+{		
+	[HideInInspector]												
+	public PathDisplayMode DisplayMode = PathDisplayMode.Connections;
+	[HideInInspector]	
+	public int UIStart 	= 0;
+	[HideInInspector]
+	public int UIEnd	= 0;
+
+	public List<Transform> Waypoints   = new List<Transform>();
+
+}
